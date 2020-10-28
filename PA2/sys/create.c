e8ea915a73bb4d7da26a9ec5ca784d61975063a8
architkwatra <akwatra@ncsu.edu>
2020-10-28 04:18:54 -0400

    dff

--- PA2/sys/create.c
+++ PA2/sys/create.c
@@ -89,21 +89,22 @@ SYSCALL create(procaddr,ssize,priority,name,nargs,args)
 	// kprintf("get_frm returned frame = %d and address = %d", t, freeFramePointer);
 	pptr->pdbr = freeFramePointer;
 	int frameId = /*t*/((int)freeFramePointer)/NBPG - FRAME0;
-	frm_tab[frameId].fr_status = 1;
-	frm_tab[frameId].fr_pid = pid;
-	frm_tab[frameId].fr_type = FR_DIR;
-	//frm_tab[frameId].fr_vpno = (int)procaddr/NBPG;
+	fr_map_t *frPtr = &frm_tab[frameId];	
+	frPtr->fr_type = FR_DIR;
+	frPtr->fr_status = 1;
+	frPtr->fr_pid = pid;
+	frm_tab[frameId].fr_vpno = (int)procaddr/NBPG;
 	
-	pd_t *directoryPointer = (pd_t*) pptr->pdbr;
+	pd_t *dPtr = (pd_t*) pptr->pdbr;
 	int j = 0;
 	while (j < 1024) {
-		directoryPointer->pd_write = 1;
+		dPtr->pd_write = 1;
 		if (j < 4) {
-			directoryPointer->pd_pres = 1;		
-			directoryPointer->pd_base = (FRAME0 + j + 1);
+			dPtr->pd_pres = 1;		
+			dPtr->pd_base = (FRAME0 + j + 1);
 		}
 
-		directoryPointer++;
+		dPtr++;
 		++j;
 	}	
 		
